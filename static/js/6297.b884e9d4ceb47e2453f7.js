7beceda93238db9943b961fb9a6adeff2f9dfb42
wkylin.w <wkylin.w@gmail.com>
2023-10-25 16:02:56 +0800

    Updates

--- static/js/6297.b884e9d4ceb47e2453f7.js
+++
@@ -1,3 +0,0 @@
-"use strict";(self.webpackChunkpro_react_admin=self.webpackChunkpro_react_admin||[]).push([[6297],{36297:function(r,e,t){var u=t(64836);Object.defineProperty(e,"__esModule",{value:!0}),e.default=void 0;var a=u(t(67294)),n=t(2110),c=function(){return a.default.createElement(a.default.Fragment,null,a.default.createElement(n.Alert,{message:"Success Tips",description:"Detailed description and advice about successful copywriting.",type:"success",showIcon:!0}))},s=c;e.default=s}}]);
-
-//# sourceMappingURL=6297.b884e9d4ceb47e2453f7.js.map
